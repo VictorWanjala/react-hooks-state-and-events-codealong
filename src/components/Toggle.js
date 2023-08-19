21eea31110431b824cb4e114179b0e6b5fb2bdf9
Victor <wanjala94vic@gmail.com>
2023-08-19 22:29:33 +0300

    completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { useState } from "react";
 function Toggle() {
-  return <button>OFF</button>;
+  const[isOn,setisOn] = useState(false);
+  
+
+  function btnHandler(event){
+    event.preventDefault()
+    setisOn(!isOn)
+  }
+  return <button onClick={btnHandler}>{isOn ? 'ON':'OFF'}</button>;
 }
 
 export default Toggle;
